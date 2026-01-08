b12f503efd854b504ecff8795b31f0d39c09b44f
Akash D P <akashdpcali@gmail.com>
2026-01-08 23:16:51 +0530

    made the calender auto update

--- calender.js
+++ calender.js
@@ -3,17 +3,23 @@ let day=document.querySelector(".day");
 let month=document.querySelector(".month");
 let year=document.querySelector(".year");
 
-let today= new Date();
+
 
 const alldays=["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 const allmonths=["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-if(today.getDate()<10){
-  date.innerText= "0"+today.getDate();
-}else{
-  date.innerText= today.getDate();
+function update(){
+  let today= new Date();
+  
+  if(today.getDate()<10){
+    date.innerText= "0"+today.getDate();
+  }else{
+    date.innerText= today.getDate();
+  }
+
+  day.innerText= alldays[today.getDay()];
+  month.innerText= allmonths[today.getMonth()];
+  year.innerText= today.getFullYear();
 }
 
-day.innerText= alldays[today.getDay()];
-month.innerText= allmonths[today.getMonth()];
-year.innerText= today.getFullYear();
+setInterval(update, 60*1000);
