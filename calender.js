b2f5eb979b63edd748adc101b10c4db511be5b52
Akash D P <akashdpcali@gmail.com>
2026-01-09 11:56:34 +0530

    called update function for instant update

--- calender.js
+++ calender.js
@@ -2,14 +2,14 @@ let date=document.querySelector(".date");
 let day=document.querySelector(".day");
 let month=document.querySelector(".month");
 let year=document.querySelector(".year");
-
+let today;
 
 
 const alldays=["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 const allmonths=["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 function update(){
-  let today= new Date();
+  today= new Date();
   
   if(today.getDate()<10){
     date.innerText= "0"+today.getDate();
@@ -21,5 +21,5 @@ function update(){
   month.innerText= allmonths[today.getMonth()];
   year.innerText= today.getFullYear();
 }
-
+update();
 setInterval(update, 60*1000);
